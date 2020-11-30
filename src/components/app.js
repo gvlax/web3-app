d1a644816c8aeb06f978c550faf03457060d597f
Maciej Gulak <maciej.gulak.extern@ergo.digital>
2020-11-30 20:16:05 +0100

    integrate waves signer

--- src/components/app.js
+++ src/components/app.js
@@ -1,36 +1,68 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
+import ReactDOM from 'react-dom'
+import React from 'react'
+import Signer from '@waves/signer'
+import Provider from '@waves.exchange/provider-web'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-        this.authFunc = this.authFunc.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.setupWaves();
+    this.authWithKeeper = this.authWithKeeper.bind(this);
+  }
 
-    authFunc() {
-        const authData = { data: "Auth on my site"};
-        if (WavesKeeper) {
-            WavesKeeper.auth(authData).then(auth => {
-                console.log(JSON.stringify(auth));
-            }).catch(error => {
-                console.error(error);
-            });
-        } else {
-            alert("To Auth WavesKeeper should be installed.");
-        }
-    }
+  setupWaves = () => {
+    this.wavesSigner = new Signer({
+      NODE_URL: 'https://nodes-testnet.wavesnodes.com',
+    })
+    this.wavesProvider = new Provider('https://testnet.waves.exchange/signer/')
+    this.wavesSigner.setProvider(this.wavesProvider)
+  }
 
-    render() {
-        return (
-            <div className="container">
-                <input className="btn btn-primary" type="submit" value="Auth" onClick={this.authFunc}/>
-            </div>
-        )
+  authWithKeeper() {
+    const authData = { data: 'Auth on my site' }
+    if (WavesKeeper) {
+      WavesKeeper.auth(authData)
+        .then((auth) => {
+          console.log(JSON.stringify(auth))
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    } else {
+      alert('To Auth WavesKeeper should be installed.')
     }
+  }
+
+  authWithSigner = async () => {
+    console.log('authUser() called ....')
+    try {
+      const userData = await this.wavesSigner.login()
+      console.log('User data: ', userData)
+    } catch (err) {
+      console.error('login rejected')
+    }
+  }
+
+  render() {
+    return (
+        <>
+            <button
+              className="btn btn-default"
+              type="submit"
+              onClick={this.authWithKeeper}
+            >Auth with WavesKeeper</button>
+            <button
+              className="btn btn-default"
+              type="submit"
+              onClick={this.authWithSigner}
+            >Auth with Signer</button>
+        </>
+    )
+  }
 }
 
-const app = document.getElementById('app');
+const app = document.getElementById('app')
 if (app) {
-    ReactDOM.render(<App/>, app);
+  ReactDOM.render(<App />, app)
 }
